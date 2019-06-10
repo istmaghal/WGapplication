301ff14dee5590bb5533e2e4cf4f8272dd744433
michaelkora <stmikora@stud.h-da.de>
2019-06-10 20:37:21 +0200

    i just made some modifications on tha file but it still working..so dont care

--- app/src/main/java/com/example/wgapplication/CustomEventListAdapter.java
+++ app/src/main/java/com/example/wgapplication/CustomEventListAdapter.java
@@ -1,17 +1,21 @@
 package com.example.wgapplication;
 
 import android.content.Context;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import android.graphics.Color;
 import java.util.ArrayList;
 
 public class CustomEventListAdapter extends ArrayAdapter<Event> {
-    public CustomEventListAdapter(Context context, ArrayList<Event> users) {
-        super(context, 0, users);
+    public CustomEventListAdapter(Context context, ArrayList<Event> myEvents) {
+        super(context, 0, myEvents);
     }
 
 
@@ -42,11 +46,15 @@ public class CustomEventListAdapter extends ArrayAdapter<Event> {
 
 
         // Populate the data into the ui using the data objects
-        name.setText(String.format(myProduct.getName()));
+        //SpannableString ss=  new SpannableString(myProduct.getName());
+        //ss.setSpan(new ForegroundColorSpan(Color.GREEN), 0, 5, 0);
+        name.setText(myProduct.getName());
         ort.setText( myProduct.getOrt() );
+
         beschreibung.setText( myProduct.getBeschreibung() );
-        datum.setText( myProduct.getTag() + "-" + myProduct.getMonat() + "-" + myProduct.getJahr() );
-        uhrzeit.setText( myProduct.getUhr() + ":" + myProduct.getMinute() );
+        String datum1="20/6/2019";
+        datum.setText(datum1  );
+        uhrzeit.setText( "15:20" );
 
         // Return the completed view to render on screen
         return convertView;
