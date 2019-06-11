08c01e3add85ddf33ca507e8a5cffe67d89b37b7
michaelkora <stmikora@stud.h-da.de>
2019-06-11 17:54:09 +0200

    hoffe es klappt nun

---
+++ app/src/main/java/com/example/wgapplication/TimePickerFragment.java
@@ -0,0 +1,23 @@
+package com.example.wgapplication;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c=Calendar.getInstance();
+        int hour=c.get(Calendar.HOUR_OF_DAY);
+        int min=c.get( Calendar.MINUTE );
+        return new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener)getActivity(),hour,min, android.text.format.DateFormat.is24HourFormat(getActivity() ) );
+    }
+}
